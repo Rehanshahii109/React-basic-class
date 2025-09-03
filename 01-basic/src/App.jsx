edba71512171dd6c0dacd02c3c7ab20b84fac2a4
Ali Rehan <rehanshahii109@gmail.com>
2025-09-03 11:17:41 +0500

    v-3

--- 01-basic/src/App.jsx
+++ 01-basic/src/App.jsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import Chai from "./chai"
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-     <h1>chai aur react with vite | ali rehan</h1>
+     <Chai/>
+     <h1>chai aur react</h1>
+     <p>test para</p>
     </>
   )
 }
