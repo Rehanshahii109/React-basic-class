0bd7305516a63f43a532f5a846ad693511b717c2
Ali Rehan <rehanshahii109@gmail.com>
2025-09-04 11:45:27 +0500

    02-hooks

---
+++ 02-counter/src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+ let [counter, setCounter] = useState(15)
+
+
+  // let Counter = 15
+
+  const addValue = ()=>{
+   
+    // counter = counter + 1
+    setCounter(counter + 1 )
+    //  console.log("clicked", counter)
+  }
+
+  const removeValue = ()=>{
+    // console.log("clicked", counter)
+    // counter = counter - 1
+    setCounter(counter - 1 )
+  }
+
+  return (
+    <>
+      <h1>chai aur react</h1>
+      <h2>Counter value: {counter}</h2>
+
+      <button
+      onClick={addValue}
+      >Add value{addValue}</button>
+      <br />
+      <button
+      onClick={removeValue}
+      >remove value {counter}</button>
+      <p>footer: {counter}</p>
+    </>
+  )
+}
+
+export default App
